7ab548e0cb4e2b0dc00d9e46c10f5510fe8ecd2c
Jan Laermann <laermannjan@gmail.com>
2017-05-26 18:00:18 +0200

    Added logger and restructured global vars.

---
+++ src/agent_vanilla.py
@@ -0,0 +1,242 @@
+import numpy as np
+import tensorflow as tf
+import random
+from collections import deque
+
+import gym
+from gym import wrappers
+
+import os
+import logging.config
+import yaml
+import datetime
+
+# Setup
+LOGGING_PATH = '../res/logging/'
+YAML_CONFIG = 'logging.yaml'
+
+ENV_NAME = 'Acrobot-v1'
+env = gym.make(ENV_NAME)
+
+# Hyperparams
+HYPER = {
+    'input_size' : env.observation_space.shape[0],
+    'output_size' : env.action_space.n,
+    'replay_memory' : 50000,
+    'minibatch_size' : 10,
+    'hidden_size' : 100,
+    'max_episodes' : 500,
+    'discount' : 0.9,
+    'learning_rate' : 1e-1
+}
+
+def setup_logging(yaml_config):
+    global config_dict
+    with open(yaml_config) as f:
+        config_dict = yaml.load(f)
+
+        # Rename logfile
+        log_filename = config_dict['handlers']['fileHandler']['filename']
+        base, extension = os.path.splitext(log_filename)
+        today = datetime.datetime.today()
+        log_filename = '{}{}{}{}'.format(
+            LOGGING_PATH,
+            ENV_NAME,
+            today.strftime('_%Y-%m-%d-%H-%M'),
+            extension)
+        config_dict['handlers']['fileHandler']['filename'] = log_filename
+
+        # Apply config
+        logging.config.dictConfig(config_dict)
+
+
+class DQN:
+    def __init__(self, session, input_size, output_size, name="main"):
+        self.session = session
+        self.input_size = input_size
+        self.output_size = output_size
+        self.net_name = name
+
+        self._build_network()
+
+    def _build_network(self, h_size=HYPER['hidden_size'], l_rate=HYPER['learning_rate']):
+        with tf.variable_scope(self.net_name):
+            self._X = tf.placeholder(tf.float32, [None, self.input_size], name="input_x")
+
+            # First layer of weights
+            W1 = tf.get_variable("W1", shape=[self.input_size, h_size],
+                                 initializer=tf.contrib.layers.xavier_initializer())
+            layer1 = tf.nn.tanh(tf.matmul(self._X, W1))
+
+            # Second layer of Weights
+            W2 = tf.get_variable("W2", shape=[h_size, self.output_size],
+                                 initializer=tf.contrib.layers.xavier_initializer())
+
+            # Q prediction
+            self._Qpred = tf.matmul(layer1, W2)
+
+        # We need to define the parts of the network needed for learning a policy
+        self._Y = tf.placeholder(shape=[None, self.output_size], dtype=tf.float32)
+
+        # Loss function
+        self._loss = tf.reduce_mean(tf.square(self._Y - self._Qpred))
+        # Learning
+        self._train = tf.train.AdamOptimizer(learning_rate=l_rate).minimize(self._loss)
+
+    def predict(self, state):
+        x = np.reshape(state, [1, self.input_size])
+        return self.session.run(self._Qpred, feed_dict={self._X: x})
+
+    def update(self, x_stack, y_stack):
+        return self.session.run([self._loss, self._train], feed_dict={self._X: x_stack, self._Y: y_stack})
+
+
+
+
+
+
+
+def replay_train(mainDQN, targetDQN, train_batch):
+    x_stack = np.empty(0).reshape(0, input_size)
+    y_stack = np.empty(0).reshape(0, output_size)
+
+    # Get stored information from the buffer
+    for state, action, reward, next_state, done in train_batch:
+        Q = mainDQN.predic(state)
+
+        # terminal?
+        if done:
+            Q[0, action] = reward
+        else:
+            # get target from target DQN (Q')
+            Q[0, action] = reward + HYPER['discount'] * np.max(targetDQN.predict(next_state))
+
+        y_stack = np.vstack([y_stack, Q])
+        x_stack = np.vstack( [x_stack, state])
+
+    # Train our network using target and predicted Q values on each episode
+    return mainDQN.update(x_stack, y_stack)
+
+def ddqn_replay_train(mainDQN, targetDQN, train_batch):
+    '''
+    Double DQN implementation
+    :param mainDQN: main DQN
+    :param targetDQN: target DQN
+    :param train_batch: minibatch for train
+    :return: loss
+    '''
+    x_stack = np.empty(0).reshape(0, mainDQN.input_size)
+    y_stack = np.empty(0).reshape(0, mainDQN.output_size)
+
+    # Get stored information from the buffer
+    for state, action, reward, next_state, done in train_batch:
+        Q = mainDQN.predict(state)
+
+        # terminal?
+        if done:
+            Q[0, action] = reward
+        else:
+            # Double DQN: y = r + gamma * targetDQN(s')[a] where
+            # a = argmax(mainDQN(s'))
+            Q[0, action] = reward + HYPER['discount'] * targetDQN.predict(next_state)[0, np.argmax(mainDQN.predict(next_state))]
+
+        y_stack = np.vstack([y_stack, Q])
+        x_stack = np.vstack([x_stack, state])
+
+    # Train our network using target and predicted Q values on each episode
+    return mainDQN.update(x_stack, y_stack)
+
+def get_copy_var_ops(*, dest_scope_name="target", src_scope_name="main"):
+
+    # Copy variables src_scope to dest_scope
+    op_holder = []
+
+    src_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=src_scope_name)
+    dest_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=dest_scope_name)
+
+    for src_var, dest_var in zip(src_vars, dest_vars):
+        op_holder.append(dest_var.assign(src_var.value()))
+
+    return op_holder
+
+def bot_play(mainDQN, env=env):
+    # See our trained network in action
+    state = env.reset()
+    reward_sum = 0
+    while True:
+        env.render()
+        action = np.argmax(mainDQN.predict(state))
+        state, reward, done, _ = env.step(action)
+        reward_sum += reward
+        if done:
+            print("Total score: {}".format(reward_sum))
+            break
+
+def main():
+    # store the previous observations in replay memory
+    replay_buffer = deque()
+
+    with tf.Session() as sess:
+        mainDQN = DQN(sess, HYPER['input_size'], HYPER['output_size'], name="main")
+        targetDQN = DQN(sess, HYPER['input_size'], HYPER['output_size'], name="target")
+        tf.global_variables_initializer().run()
+
+        #initial copy q_net -> target_net
+        copy_ops = get_copy_var_ops(dest_scope_name="target", src_scope_name="main")
+        sess.run(copy_ops)
+
+        for episode in range(HYPER['max_episodes']):
+            e = 1. / ((episode / 10) + 1)
+            done = False
+            step_count = 0
+            state = env.reset()
+
+            while not done:
+                if np.random.rand(1) < e:
+                    action = env.action_space.sample()
+                else:
+                    # Choose an action by greedily from the Q-network
+                    action = np.argmax(mainDQN.predict(state))
+
+                # Get new state and reward from environment
+                next_state, reward, done, _ = env.step(action)
+
+                # Save the experience to our buffer
+                replay_buffer.append((state, action, reward, next_state, done))
+                if len(replay_buffer) > HYPER['replay_memory']:
+                      replay_buffer.popleft()
+
+                state = next_state
+                step_count += 1
+                if step_count > 10000:   # Good enough. Let's move on
+                    break
+
+            print("Episode: {} steps: {}".format(episode, step_count))
+            if step_count > 10000:
+                pass
+                # break
+
+            if episode % 10 == 1: # train every 10 episode
+                # Get a random batch of experiences
+                for _ in range(50):
+                    minibatch = random.sample(replay_buffer, HYPER['minibatch_size'])
+                    loss, _ = ddqn_replay_train(mainDQN, targetDQN, minibatch)
+
+                print("Loss: ", loss)
+                # copy q_net -> target_net
+                sess.run(copy_ops)
+
+        # See our trained bot in action
+        env2 = wrappers.Monitor(env, 'gym-results', force=True)
+
+        for i in range(200):
+            bot_play(mainDQN, env=env2)
+
+        env2.close()
+        # gym.upload("gym-results", api_key="sk_VT2wPcSSOylnlPORltmQ")
+
+if __name__ == "__main__":
+    setup_logging(LOGGING_PATH + YAML_CONFIG)
+    logger = logging.getLogger()
+    logger.info('Starting Test with hyperparameters: %s,', HYPER)
+    main()
