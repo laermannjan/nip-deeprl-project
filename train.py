bc8bb445d3058b5bf28e25bd41dc582983017b81
Jan Laermann <laermannjan@gmail.com>
2017-06-07 07:27:39 +0200

    We now use deepq.learn and can pickle trained agents.
    Train an agent with train.py given a experiment configuration you can
    specify in configs.py. train.py will run your experiment and pickle your
    agent after the experiment so you can reuse the trained agent later. It
    also creates checkpoints such that it remembers the most successful
    q-func approximation and not just the very last.
    
    Use enjoy.py to select the trained agent (pickle-file) and specify the
    environment you want it to play.

---
+++ train.py
@@ -0,0 +1,48 @@
+import gym
+
+from baselines import deepq
+import tensorflow as tf
+import tensorflow.contrib.layers as layers
+from collections import namedtuple
+import os
+
+from configs import Configs
+
+
+def is_solved_cartpole(lcl, glb, min_t_solved, mean_window, min_mean_reward):
+    is_solved = lcl['t'] > min_t_solved and sum(lcl['episode_rewards'][-(mean_window+1):-1]) / mean_window >= min_mean_reward
+    return is_solved
+
+def is_solved_func(lcl, glb):
+    return is_solved_cartpole(lcl, glb, config.min_t_solved, config.mean_window, config.min_mean_reward)
+
+
+if __name__ == '__main__':
+    config = Configs['cartpole_basic']
+    print(config.learning_rate)
+
+    env = gym.make(config.env)
+    act = deepq.learn(
+        env,
+        q_func=deepq.models.mlp(config.num_nodes),
+        lr=config.learning_rate,
+        max_timesteps=config.max_timesteps,
+        buffer_size=config.replay_buffer_size,
+        exploration_fraction=config.exploration_fraction,
+        exploration_final_eps=config.final_p,
+        train_freq=config.train_freq,
+        batch_size=config.minibatch_size,
+        print_freq=config.print_freq,
+        checkpoint_freq=config.checkpoint_freq,
+        learning_starts=config.learning_delay,
+        gamma=config.gamma,
+        target_network_update_freq=config.update_freq,
+        callback=is_solved_func
+    )
+
+    pickle_dir = 'trained_agents'
+    if not os.path.exists(pickle_dir):
+        os.makedirs(pickle_dir)
+    pickle_fname = 'cartpole_model.pkl' # TODO: Need to add some sort of id, maybe timestamp
+    print("Saving model as {}".format(pickle_fname))
+    act.save(os.path.join(pickle_dir,pickle_fname))
