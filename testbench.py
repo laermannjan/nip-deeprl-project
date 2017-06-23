0228be08eeedc153754d540cfcaa16adb55b5fe8
Jan Laermann <laermannjan@gmail.com>
2017-06-23 02:28:05 +0200

    Testbench updated according to new training facility.
    testbench.py now utilizes the new training facility.
    Regular usuage: passing a number of configs and repeat counter is still
    available. Additionally, several options have been added such as video
    capturing, writing stats to file before the job has been finished and many more.
    Check out the `--help` option.

--- testbench.py
+++ testbench.py
@@ -1,75 +1,80 @@
-import argparse
 import os
-import datetime
+import time
+import argparse
+from gym import envs
+from baselines.common.misc_util import boolean_flag
+from project_framework.training import train
+from dummy import Configs
 
-from baselines import logger
-import baselines.common.tf_util as U
+def parse_args():
+    parser = argparse.ArgumentParser("DQN experiments for OpenAI Gym games")
+    # Environment
+    parser.add_argument("--env", type=str, choices=[spec.id for spec in envs.registry.all()], default="LunarLander-v2", help="name of the game")
+    parser.add_argument("--seed", type=int, default=42, help="which seed to use")
+    # Core DQN parameters
+    parser.add_argument("--replay-buffer-size", type=int, default=int(1e6), help="replay buffer size")
+    parser.add_argument("--lr", type=float, default=1e-4, help="learning rate for Adam optimizer")
+    parser.add_argument("--num-steps", type=int, default=int(2e8), help="total number of steps to run the environment for")
+    parser.add_argument("--batch-size", type=int, default=32, help="number of transitions to optimize at the same time")
+    parser.add_argument("--learning-freq", type=int, default=4, help="number of iterations between every optimization step")
+    parser.add_argument("--target-update-freq", type=int, default=40000, help="number of iterations between every target network update")
+    parser.add_argument("--arch", type=int, default=[256]*3, nargs='+', help="number of nodes per layer for this model.")
+    parser.add_argument("--gamma", type=float, default=0.99, help="DQN discount factor")
+    parser.add_argument("--schedule-timesteps", type=int, default=150000, help="steps in which exploration fraction anneals from initial_p to final_p.")
+    parser.add_argument("--initial-p", type=float, default=1.0, help="intial p")
+    parser.add_argument("--final-p", type=float, default=0.01, help="final p")
+    # Bells and whistles
+    boolean_flag(parser, "double-q", default=True, help="whether or not to use double q learning")
+    boolean_flag(parser, "dueling", default=False, help="whether or not to use dueling model")
+    boolean_flag(parser, "prioritized", default=False, help="whether or not to use prioritized replay buffer")
+    parser.add_argument("--prioritized-alpha", type=float, default=0.6, help="alpha parameter for prioritized replay buffer")
+    parser.add_argument("--prioritized-beta0", type=float, default=0.4, help="initial value of beta parameters for prioritized replay")
+    parser.add_argument("--prioritized-eps", type=float, default=1e-6, help="eps parameter for prioritized replay buffer")
+    # Checkpointing
+    parser.add_argument("--save-dir", type=str, default='experiments', help="directory in which training state and model should be saved.")
+    parser.add_argument("--save-freq", type=int, default=1e6, help="save model once every time this many iterations are completed")
+    boolean_flag(parser, "load-on-start", default=True, help="if true and model was previously saved then training will be resumed")
+    # Augmentations
+    parser.add_argument("--augmented-reward", type=int, default=None, help="augmented reward when environment is done.")
+    parser.add_argument("--max-episode-steps", type=int, default=None, help="augmented max length of each episode.")
+    # Monitoring
+    boolean_flag(parser, "write-upon-reset", default=False, help="if true results get written to disk after every episode. this might severly slow down the process.")
+    boolean_flag(parser, "capture-videos", default=False, help="if true enables video capturing.")
+    parser.add_argument("--run-num", default=None, help="identifies the run number if experiment is run mulitple times.")
+    # Config
+    parser.add_argument("--config", type=str, nargs='+', choices=Configs.keys(), default=None, help="define a config by name from configs.py which may overwrite other arguments")
+    parser.add_argument("--repeat", type=int, default=1, help="number of times the same experiment is being repeated. if multiple configs are defined, each is being repeated individually.")
+    return parser.parse_args()
 
-from configs import Configs
-from project_framework.training import CustomTrainer
+def _load_config(args, config):
+    for attr, val in Configs[config].items():
+        setattr(args, attr, val)
 
-
+def load_config(args, config):
+    # Load defaults
+    if Configs[config]['env'] == 'Cartpole-v0':
+        _load_config(args, 'CP_basic')
+    elif Configs[config]['env'] == 'LunarLander-v2':
+        _load_config(args, 'LL_basi')
+    elif Configs[config]['env'] == 'Acrobot-v1':
+        _load_config(args, 'AB_basic')
+    # Load modifications
+    _load_config(args, config)
+    # create subdir for config
+    setattr(args, 'save_dir', os.path.join(args.save_dir, config))
 
 if __name__ == '__main__':
-    # get rid of TF warnings in the beginning
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    parser = argparse.ArgumentParser(prog='custom_train',
-                                     description='Train agent to play an OpenAI gym env.')
-    parser.add_argument('env',
-                        action='store',
-                        choices=['Cartpole-v0', 'LunarLander-v2', 'Acrobot-v1'],
-                        metavar='ENV',
-                        type=str,
-                        help='OpenAI Gym Id of the environment.')
-    parser.add_argument('configs',
-                        action='store',
-                        nargs='+',
-                        choices=Configs.keys(),
-                        metavar='CONFIG',
-                        type=str,
-                        help='Name of config from configs.py.')
-    parser.add_argument('--repeat',
-                        action='store',
-                        dest='repeat',
-                        type=int,
-                        default=1,
-                        help='Amount of times an experiment should be repeated.')
-    parser.add_argument('--enable-videos',
-                        action='store_true',
-                        dest='enable_videos',
-                        help='Toggle to turn on video capturing.')
-    parser.add_argument('--dir',
-                        action='store',
-                        type=str,
-                        dest='root_dir',
-                        default='experiments',
-                        help='Root directory for this experiment.')
-    parser.add_argument('--cores',
-                        action='store',
-                        type=int,
-                        dest='num_cpu',
-                        default=8,
-                        help='Number of cpu cores to be used for training.')
-    parser.add_argument('--name',
-                        action='store',
-                        type=str,
-                        dest='exp_name',
-                        default=os.getpid(),
-                        help='Name of this experiment.')
-    args = parser.parse_args()
-    for i, config_name in enumerate(args.configs):
-        logger.log('#'*35 + '\n' +
-                   'Performing [{}/{}] experiments...\n'.format(i+1, len(args.configs)) +
-                   '#'*35)
-        for run in range(args.repeat):
-            logger.log('::: Performing Run [{}/{}]...\n '.format(run+1, args.repeat))
-            root_dir = os.path.join(args.root_dir, args.env, config_name)
-            videos_enabled = None if run == 0 and args.enable_videos else False
-            trainer = CustomTrainer(env_id=args.env,
-                                    config_name=config_name,
-                                    root_dir=root_dir,
-                                    exp_name=args.exp_name,
-                                    videos_enabled=videos_enabled,
-                                    num_cpu=args.num_cpu)
-            trainer.train()
-            U.reset()
+    orig_args = parse_args()
+
+    if orig_args.config is not None:
+        for config in orig_args.config:
+            args = orig_args
+            load_config(orig_args, config)
+            for _ in range(orig_args.repeat):
+                # Train experiment
+                train(args)
+    else:
+        args = orig_args
+        setattr(args, 'save_dir', os.path.join(args.save_dir, 'TEST', str(time.time())))
+        for _ in range(orig_args.repeat):
+            train(args)
