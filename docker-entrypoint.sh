b062e73c339835f436c0aa8f4a3cb4e23e82a161
Jan Laermann <laermannjan@gmail.com>
2017-07-02 15:49:15 +0200

    Fixed screen resolution and made write-upon-reset default.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 tstamp=$(date +"%s")
-xvfb-run -e /mnt/data/$tstamp.xvfb.err -a -s "-screen 0 800x600x16"\
-         -- python $PROJECT_PATH/testbench.py --save-dir /mnt/data $@ > /mnt/data/$tstamp.experiment.log 2>&1
+xvfb-run -e /mnt/data/$tstamp.xvfb.err -a -s "-screen 0 1400x900x24"\
+         -- python $PROJECT_PATH/testbench.py --save-dir /mnt/data --write-upon-reset $@ > /mnt/data/$tstamp.experiment.log 2>&1
