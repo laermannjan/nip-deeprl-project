2a3c2fa8a31ffe97a063340430ae741908c5a87c
Jan Laermann <laermannjan@gmail.com>
2017-07-02 06:34:46 +0200

    Added new Dockerfiles for CPU and GPU version.
    Video rendering does not yet work on GPUs.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-source activate py35
 tstamp=$(date +"%s")
-xvfb-run -e /mnt/data/$tstamp.xvfb.err -a -s "-screen 0 1400x900x24"\
+xvfb-run -e /mnt/data/$tstamp.xvfb.err -a -s "-screen 0 800x600x16"\
          -- python $PROJECT_PATH/testbench.py --save-dir /mnt/data $@ > /mnt/data/$tstamp.experiment.log 2>&1
