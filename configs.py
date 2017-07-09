d82a40d2db639105b3d1cd86463df25b13ae1325
bordeauxred <robert.mueller@outlook.de>
2017-07-09 18:05:11 +0200

    added configs for AB configs

--- configs.py
+++ configs.py
@@ -1,21 +1,21 @@
 Configs = {
     'AB_basic': {
         'env': 'Acrobot-v1',
-        'arch': [64, 64], #prev 3 layer
-        'gamma': 0.99,
-        'lr':5e-3,
-        'num_steps': 500*6000,
+		'arch': [64],
+       	'gamma': 0.99,
+        'lr':5e-4,
+        'num_steps': 500*200,
         'num_episodes': 500*20,
-        'max_episode_steps': 500, #prev 3000
-        'schedule_timesteps': 500*100, #prev 3000*100
+        'max_episode_steps': 500,
+        'schedule_timesteps': 500*20,
         'initial_p': 1.0,
-        'final_p': 0.1,
-        'learning_freq': 1,
-        'target_update_freq': 500*30,# prev 3000*30,
-        'replay_buffer_size': 500*20, # prev 3000*20,
-        'batch_size': 50,
+        'final_p': 0.01,
+        'learning_freq': 1, # critical parameter
+        'target_update_freq': 500*2,
+        'replay_buffer_size': 500*100,
+        'batch_size': 32,
         'augmented_reward': None,
-        'save_freq': 500*20 # prev 3000*20,
+        'save_freq': 500*20,
     },
     'CP_basic': {
         'env': 'CartPole-v0',
@@ -53,7 +53,7 @@ Configs = {
         'save_freq': 500*20,
     },
 	#larger exploration (500*500)
-    'LL_e500': {   
+    'LL_e500': {
 		'env': 'LunarLander-v2',
         'schedule_timesteps': 500*500, # 10% of approx. all steps within num_episodes
     },
@@ -153,7 +153,7 @@ Configs = {
         'env': 'Acrobot-v1',
 	    'num_episodes': 500*4,
 		'arch': [64],
-    },	
+    },
 	# search a stable AB basic
     'AB_64-64-1000': {
         'env': 'Acrobot-v1',
@@ -190,30 +190,27 @@ Configs = {
 		'max_episode_steps': 1000,
     },
 
-    ##########################################################################
-    #AB - Configs, assuming we find a stable AB_basic with 'max_episode_steps': 500
-    ################################################################
     #larger exploration (500*500)
       'AB_e500': {
       'env': 'Acrobot-v1',
           'schedule_timesteps': 500*1000, # 10% of approx. all steps within num_episodes, assume 500 steps per episode, prev. 500*500
       },
     #larger Replaybuffer (350*500)
-      'AB_rpb150': {
-          'env': 'Acrobot-v1',
-          'replay_buffer_size': 500*300
-      },
-    #larger replaybuffer (600*500)
       'AB_rpb300': {
           'env': 'Acrobot-v1',
           'replay_buffer_size': 500*600
       },
-    #clip gradients (10)
+    #larger replaybuffer (600*500)
+      'AB_rpb600': {
+          'env': 'Acrobot-v1',
+          'replay_buffer_size': 1000*600
+      },
+    #clip gradients (10) --optional
       'AB_gc10': {
           'env': 'Acrobot-v1',
           'grad_clip': 10 ,
       },
-    #clip gradients (5)
+    #clip gradients (5) --optional
       'AB_gc5': {
           'env': 'Acrobot-v1',
           'grad_clip': 5 ,
@@ -222,48 +219,46 @@ Configs = {
       'AB_prio1': {
           'env': 'Acrobot-v1',
           'prioritized': True,
-      'prioritized_alpha': 0.2,
+      'prioritized_alpha': 0.1,
       'prioritized_beta0': 0.5,
       },
     #priotizes sampling (alpha 0.8)
       'AB_prio2': {
           'env': 'Acrobot-v1',
           'prioritized': True,
-      'prioritized_alpha': 0.8,
+      'prioritized_alpha': 0.9,
       'prioritized_beta0': 0.5,
       },
-    # examine architectures after finding a stable basic configuration
-    # #2nd layer (64-64)
-    #   'AB_64_64': {
-    #       'env': 'Acrobot-v1',
-    #       'arch': [64, 64],
-    #   },
-    # #2nd layer (64-32)
-    #   'AB_64_32': {
-    #       'env': 'Acrobot-v1',
-    #       'arch': [64, 32],
-    #   },
-    # #2nd layer (64-96)
-    #   'AB_64_96': {
-    #       'env': 'Acrobot-v1',
-    #       'arch': [64, 96],
-    #   },
-
-
-      'AB_frqz50_smp100': {
+    #one smaller layer (32)
+      'AB_32': {
           'env': 'Acrobot-v1',
-          'learning_freq': 50,
-      'num_samples': 100,
+          'arch': [32],
       },
-    # update more (100 batches /1 steps)
-      'AB_frqz1_smp100': {
+    #one bigger layer (128)
+      'AB_32': {
           'env': 'Acrobot-v1',
-      'num_samples': 100,
+          'arch': [128],
       },
+    #2nd layer (64-64)
+      'AB_64_64': {
+          'env': 'Acrobot-v1',
+          'arch': [64, 64],
+      },
+    #2nd layer (64-32)
+      'AB_64_32': {
+          'env': 'Acrobot-v1',
+          'arch': [64, 32],
+      },
+    #2nd layer (64-96)
+      'AB_64_96': {
+          'env': 'Acrobot-v1',
+          'arch': [64, 96],
+      },
+
       # extended exploration + larger rp buffer
-      'AB_e500_rpb600': {
+      'AB_e500_rpb300': {
           'env': 'Acrobot-v1',
-          'schedule_timesteps': 500*500,
+          'schedule_timesteps': 500*1000,
           'replay_buffer_size' : 500*600
       },
       # heavy gradient clipping
